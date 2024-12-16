e611da7d404d3de4865a479664ccf4463bdbc7fd
mag <polina.arutyunyan.08@mail.ru>
2024-12-16 21:36:23 +0300

    fix modal controller

--- src/modalController.js
+++ src/modalController.js
@@ -1,19 +1,9 @@
-import { renderModal } from '../view/modal.js';
-
 export default (watchedState, id) => {
   const post = watchedState.posts.find((statePost) => statePost.postId === id);
   const { postId } = post;
 
-  if (watchedState.uiState.visitedPosts.length) {
-    if (!watchedState.uiState.visitedPosts.includes(postId)) {
-      watchedState.uiState.currentPostId = postId;
-      watchedState.uiState.visitedPosts.push(postId);
-    } else {
-      watchedState.uiState.currentPostId = postId;
-      renderModal(watchedState);
-    }
-  } else {
-    watchedState.uiState.currentPostId = postId;
+  watchedState.uiState.currentPostId = postId;
+  if (!watchedState.uiState.visitedPosts.includes(postId)) {
     watchedState.uiState.visitedPosts.push(postId);
   }
 };
